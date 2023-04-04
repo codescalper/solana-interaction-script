8e36a3ba1c26c2688950b82db8399caac4ef1917
codescalper <msfunbook@gmail.com>
2023-04-04 12:25:49 +0530

    Added import statements

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+//Addng import statements
+
+import * as Web3 from '@solana/web3.js';
+import * as fs from 'fs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 async function main() {}
 
 main()
