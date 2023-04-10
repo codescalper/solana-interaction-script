474d59985a2a3bbb9ed1afb171187848c83b1b1f
codescalper <msfunbook@gmail.com>
2023-04-10 19:13:59 +0530

    Documented code with comments for future reference

--- src/index.ts
+++ src/index.ts
@@ -22,15 +22,16 @@ async function main() {
 async function initializeKeypair(connection: Web3.Connection): Promise<Web3.Keypair> {
   if (!process.env.PRIVATE_KEY) {
     console.log('Generating new keypair... 🗝️');
-    const signer = Web3.Keypair.generate();
+    const signer = Web3.Keypair.generate();  //private key is used to sign the transaction
 
-    console.log('Creating .env file');
-    fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
+    console.log('Creating .env file');    //.env files contain key value pair -- The purpose is to store the sensitive and config specific data structores
+    fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`); //fs (file system) module is used to interact with the file system in the computer 
+    //and it takes two paramter name of the file and content of the file
 
     return signer;
   }
 
-  const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];
+  const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];//If an existing PRIVATE_KEY environment variable is found, it is parsed from a JSON string into a number array and converted into a Uint8Array.
   const secretKey = Uint8Array.from(secret);
   const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
   return keypairFromSecret;
@@ -46,8 +47,8 @@ async function pingProgram(connection: Web3.Connection, payer: Web3.Keypair) {
     keys: [
       {
         pubkey: PROGRAM_DATA_PUBLIC_KEY,
-        isSigner: false,
-        isWritable: true
+        isSigner: false,  //writer doesn't require signature from data accounts
+        isWritable: true  //cause the account is being written
       }
     ],
     
@@ -57,11 +58,17 @@ async function pingProgram(connection: Web3.Connection, payer: Web3.Keypair) {
     // 3. Data - in this case, there's none!
   })
 
+  // Here we,
+  // we make a transaction
+  // we make an instruction
+  // we add the instruction to the transaction
+  // we send the transaction to the network!
+
   transaction.add(instruction)
-  const transactionSignature = await Web3.sendAndConfirmTransaction(connection, transaction, [payer])
+  const transactionSignature = await Web3.sendAndConfirmTransaction(connection, transaction, [payer]) 
 
   console.log(
-    `Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    `Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet` //check the status of our transaction on solana explorer
     )
 }
 
@@ -69,16 +76,16 @@ async function airdropSolIfNeeded(
   signer: Web3.Keypair,
   connection: Web3.Connection
 ) {
-  const balance = await connection.getBalance(signer.publicKey);
+  const balance = await connection.getBalance(signer.publicKey); //self ezplanatory
   console.log('Current balance is', balance / Web3.LAMPORTS_PER_SOL, 'SOL');
 
   
-  if (balance / Web3.LAMPORTS_PER_SOL < 1) {
+  if (balance / Web3.LAMPORTS_PER_SOL < 1) { //if balance is less than one sol here i have taken one sol because the transaction fees for solana is preety less and 1 sol is enough for making any transaction  :)
     
     console.log('Airdropping 1 SOL');
     const airdropSignature = await connection.requestAirdrop(
       signer.publicKey,
-      Web3.LAMPORTS_PER_SOL
+      Web3.LAMPORTS_PER_SOL 
     );
 
     const latestBlockhash = await connection.getLatestBlockhash();
