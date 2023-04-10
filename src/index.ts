37492fceda79369953c6117f85eebf7a2a70ca32
codescalper <msfunbook@gmail.com>
2023-04-10 18:04:16 +0530

    Added

--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,66 @@ import * as Web3 from '@solana/web3.js';
 import * as fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
-
-//initializeKeypair function which will create a keypair if one don't have it 
-
-async function initializeKeypair(connection: Web3.Connection): Promise<Web3.Keypair> {
-    if (!process.env.PRIVATE_KEY) {
-      console.log('Generating new keypair... 🗝️');
-      const signer = Web3.Keypair.generate();
-  
-      console.log('Creating .env file');
-      fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
-  
-      return signer;
-    }
-  
-    const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];
-    const secretKey = Uint8Array.from(secret);
-    const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
-    return keypairFromSecret;
-  }
+const PROGRAM_ID = new Web3.PublicKey("ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa")
+const PROGRAM_DATA_PUBLIC_KEY = new Web3.PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod")
 
 async function main() {
     const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
     const signer = await initializeKeypair(connection);
   
     console.log("Public key:", signer.publicKey.toBase58());
-    await airdropSolIfNeeded(signer, connection);
+    // await airdropSolIfNeeded(signer, connection);
+    await pingProgram(connection, signer)
+    
 }
 
+//initializeKeypair function which will create a keypair if one don't have it 
+
+async function initializeKeypair(connection: Web3.Connection): Promise<Web3.Keypair> {
+  if (!process.env.PRIVATE_KEY) {
+    console.log('Generating new keypair... 🗝️');
+    const signer = Web3.Keypair.generate();
+
+    console.log('Creating .env file');
+    fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
+
+    return signer;
+  }
+
+  const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];
+  const secretKey = Uint8Array.from(secret);
+  const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
+  return keypairFromSecret;
+}
+
+async function pingProgram(connection: Web3.Connection, payer: Web3.Keypair) {
+  
+  const transaction = new Web3.Transaction()
+  const instruction = new Web3.TransactionInstruction({
+    // Instructions need 3 things 
+    
+    // 1. The public keys of all the accounts the instruction will read/write
+    keys: [
+      {
+        pubkey: PROGRAM_DATA_PUBLIC_KEY,
+        isSigner: false,
+        isWritable: true
+      }
+    ],
+    
+    // 2. The ID of the program this instruction will be sent to
+    programId: PROGRAM_ID
+    
+    // 3. Data - in this case, there's none!
+  })
+
+  transaction.add(instruction)
+  const transactionSignature = await Web3.sendAndConfirmTransaction(connection, transaction, [payer])
+
+  console.log(
+    `Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    )
+}
 
 async function airdropSolIfNeeded(
   signer: Web3.Keypair,
